6fc9ccae7285bfa49f4300bc4cdabdf56e0d384d
bobacer <musgravehill@gmail.com>
2021-03-14 12:38:50 +0300

    stk

---
+++ STKadapterArduinoNano/STK-forum_files/adjsc.php
@@ -0,0 +1,7 @@
+function phpads_deliverActiveX(content) {document.write(content);}
+var OA_output = new Array();
+OA_output['400x25,_campaignid:2850'] = '';
+OA_output['zone:182'] = '';
+OA_output['zone:182'] += '<'+'div style="width:100%; text-align:center;">\n<'+'div id="RCGroups_com_728x90_NetworkBackfill_COL_Forum" style="width:auto; height:auto;">\n    <'+'script type="text/javascript">\n    vsQueueAd("RCGroups_com_728x90_NetworkBackfill_COL_Forum");\n    <'+'/script>\n<'+'/div>\n<'+'/div>';
+OA_output['zone:98'] = '';
+OA_output['clientid=1004'] = '';
